857fbe7c6f7bb3da983d97676f927cd9f37044bc
ayan <ayanghosh1389@gmail.com>
2023-04-16 14:46:52 +0530

    project link added

--- src/components/myprojects/Projects.jsx
+++ src/components/myprojects/Projects.jsx
@@ -6,32 +6,44 @@ import moviesnap from "../../assets/movie.webp";
 import todosnap from "../../assets/todoApp.webp";
 import librarysnap from "../../assets/library.webp";
 import keepersnap from "../../assets/keeperApp.webp";
+import ytdownloadsnap from "../../assets/ytdownload.webp";
 
 function Projects() {
   const [projects, setProjects] = useState([
     {
+      url: "#",
       image: ecomsnap,
       title: "E-commerce(mern)",
       details: [],
     },
     {
+      url: "https://ayan-movie.netlify.app/",
       image: moviesnap,
       title: "Movie search (React js)",
       details: [],
     },
     {
+      url: "https://ayan-todo-list.netlify.app/",
       image: todosnap,
       title: "TodoApp (React js)",
       details: [],
     },
     {
+      url: "https://library-ayan.netlify.app/",
       image: librarysnap,
       title: "Library (React Redux)",
       details: [],
     },
     {
+      url: "https://ayan-keeper.netlify.app/",
       image: keepersnap,
       title: "KeeperApp (React js)",
+      details: [],
+    },
+    {
+      url: "https://yt-download-ayan.netlify.app/",
+      image: ytdownloadsnap,
+      title: "Youtube video downloder (React js)",
       details: [],
     },
   ]);
@@ -45,7 +57,12 @@ function Projects() {
       </div>
       <div className="p-div">
         {projects.map((project, index) => (
-          <ProjectCard key={index} img={project.image} title={project.title} />
+          <ProjectCard
+            key={index}
+            img={project.image}
+            title={project.title}
+            url={project.url}
+          />
         ))}
       </div>
     </div>
--- src/components/myprojects/projectCard/ProjectCard.jsx
+++ src/components/myprojects/projectCard/ProjectCard.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./ProjectCard.css";
 function ProjectCard(props) {
   return (
-    <div className="p-card">
-      <div className="thbnail">
-        <img className="card-img" src={props.img} alt="" />
-        <div className="overlay">{props.title}</div>
+    <a href={props.url} target="blank">
+      <div className="p-card">
+        <div className="thbnail">
+          <img className="card-img" src={props.img} alt="" />
+          <div className="overlay">{props.title}</div>
+        </div>
+        <button className="btn">More details</button>
       </div>
-      <button className="btn">More details</button>
-    </div>
+    </a>
   );
 }
 
