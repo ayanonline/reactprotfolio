b20d3d65b4f5f4a2a4650feebf261a4b9fe715a5
ayan <ayanghosh1389@gmail.com>
2023-06-06 00:10:24 +0530

    project images changed

--- src/components/myprojects/projectsData.js
+++ src/components/myprojects/projectsData.js
@@ -1,29 +1,24 @@
 import ytdownload from "../../assets/ytdownload.webp";
-import ytdownloadPhone from "../../assets/ytdownload-phone.webp";
 import movie from "../../assets/movie.webp";
-import moviePhone from "../../assets/movie-phone.webp";
 import kgf from "../../assets/kgf.webp";
-import kgfPhone from "../../assets/kgf-phone.webp";
+
 const projects = [
   {
     title: "Youtube video Downloader",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id, ut odio quae atque maxime nobis possimus recusandae facere repellat hic illum corporis, maiores temporibus minus nisi mollitia nostrum sed?",
-    desktopImage: ytdownload,
-    mobileImage: ytdownloadPhone,
+    image: ytdownload,
     link: "https://yt-download-ayan.netlify.app/",
   },
   {
     title: "Movie serch",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id, ut odio quae atque maxime nobis possimus recusandae facere repellat hic illum corporis, maiores temporibus minus nisi mollitia nostrum sed?",
-    desktopImage: movie,
-    mobileImage: moviePhone,
+    image: movie,
     link: "https://ayan-movie.netlify.app/",
   },
   {
     title: "GYM",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id, ut odio quae atque maxime nobis possimus recusandae facere repellat hic illum corporis, maiores temporibus minus nisi mollitia nostrum sed?",
-    desktopImage: kgf,
-    mobileImage: kgfPhone,
+    image: kgf,
     link: "https://kgf.netlify.app/",
   },
 ];
--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -8,31 +8,15 @@ import mobile from "../../assets/mobile.png";
 function ProjectCard({ project }) {
   return (
     <div className="Project-card">
-      <div className="frame">
-        <div
-          className="desktop-frame"
-          style={{
-            backgroundImage: `url(${desktopFrame})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "contain",
-            backgroundPosition: "center",
-            width: "100%",
-          }}
-        >
-          <img id="desktop-image" src={project?.desktopImage} alt="desktop" />
-        </div>
-        <div
-          className="mobile-frame"
-          style={{
-            backgroundImage: `url(${phoneFrame})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "contain",
-            backgroundPosition: "center",
-          }}
-        >
-          <img id="mobile-image" src={project?.mobileImage} alt="mobile" />
-        </div>
-      </div>
+      <div
+        className="frame"
+        style={{
+          backgroundImage: `url(${project.image})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+        }}
+      ></div>
       <div className="about-project">
         <h2>{project?.title}</h2>
         <p>{project?.desc}</p>
