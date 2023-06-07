de182f1538367adf278a1193059afd3fc3290f15
ayan <ayanghosh1389@gmail.com>
2023-06-07 09:52:17 +0530

    project desc changed

--- src/components/myprojects/projectsData.js
+++ src/components/myprojects/projectsData.js
@@ -5,13 +5,13 @@ import kgf from "../../assets/kgf.webp";
 const projects = [
   {
     title: "Youtube video Downloader",
-    desc: "I have created this youtube video downloader app using RECT JS & EXPRESS. in backend i use ytdl-core npm package.",
+    desc: `I have created this youtube video downloader app using REACT JS & EXPRESS . In backend I used "ytdl-core" npm package.`,
     image: ytdownload,
     link: "https://yt-download-ayan.netlify.app/",
   },
   {
     title: "Movie serch",
-    desc: "I build this using siple free movie api, where you can serach movie by it's name. It also have add to favourite feature.",
+    desc: `I build this using simple free movie api, where you can serach movie by it's name. It also have "add to favourite" feature.`,
     image: movie,
     link: "https://ayan-movie.netlify.app/",
   },
