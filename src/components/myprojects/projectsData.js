578abf7996ea8974b6ee1c6e6e290a344f14cd6e
ayan <ayanghosh1389@gmail.com>
2023-06-07 09:48:54 +0530

    project desc changed

--- src/components/myprojects/projectsData.js
+++ src/components/myprojects/projectsData.js
@@ -5,19 +5,19 @@ import kgf from "../../assets/kgf.webp";
 const projects = [
   {
     title: "Youtube video Downloader",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id, ut odio quae atque maxime nobis possimus recusandae facere repellat hic illum corporis, maiores temporibus minus nisi mollitia nostrum sed?",
+    desc: "I have created this youtube video downloader app using RECT JS & EXPRESS. in backend i use ytdl-core npm package.",
     image: ytdownload,
     link: "https://yt-download-ayan.netlify.app/",
   },
   {
     title: "Movie serch",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id, ut odio quae atque maxime nobis possimus recusandae facere repellat hic illum corporis, maiores temporibus minus nisi mollitia nostrum sed?",
+    desc: "I build this using siple free movie api, where you can serach movie by it's name. It also have add to favourite feature.",
     image: movie,
     link: "https://ayan-movie.netlify.app/",
   },
   {
     title: "GYM",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id, ut odio quae atque maxime nobis possimus recusandae facere repellat hic illum corporis, maiores temporibus minus nisi mollitia nostrum sed?",
+    desc: "I have created fronted of this GYM website.",
     image: kgf,
     link: "https://kgf.netlify.app/",
   },
